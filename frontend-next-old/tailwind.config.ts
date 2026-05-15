9867af15862edea2191477820581dcacc988b6eb
Ducanh2005HUST <ducanheya@gmail.com>
2026-05-15 18:57:59 +0700

    structure project

--- frontend-next-old/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './styles/**/*.{css}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
